1e2b7760aaa65d1cbbd07c3ce7ebc895caf74091
ENYDAPS <88312902+aizugurin@users.noreply.github.com>
2024-06-25 21:05:32 +0800

    Create akun.php

---
+++ admin/akun.php
@@ -0,0 +1,56 @@
+<?php
+include '../assets/conn/config.php';
+if (isset($_GET['aksi'])) {
+    if ($_GET['aksi']=='ubah') {
+        $id_akun = $_POST['id_akun'];
+        $nama_lengkap = $_POST['nama_lengkap'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        mysqli_query($conn,"UPDATE tbl_akun SET nama_lengkap='$nama_lengkap', username='$username',
+            password='$password' WHERE id_akun='$id_akun'");
+        header("location:index.php");
+
+    }
+}   
+include 'header.php';
+?>
+
+<div class="container">
+	<div class="card shadow p-5 mb-5">
+        <div class="card-header">
+            <h5 class="m-0 font-weight-bold text-primary">Akun</h5>
+        </div>
+
+        <div class="card-body">
+            <?php
+            $username = $_SESSION['username'];
+            $data = mysqli_query($conn,"SELECT * FROM tbl_akun WHERE username='$username'");
+            $a = mysqli_fetch_array($data)
+            ?>
+            <form action="akun.php?aksi=ubah" method="POST">
+                <input type="hidden" name="id_akun" class="form-control" value="<?=$a['id_akun']?>">
+
+                <div class="form-group">
+                    <label>Nama Lengkap</label>
+                    <input type="text" name="nama_lengkap" class="form-control" value="<?=$a['nama_lengkap']?>">
+                </div>
+                <div class="form-group">
+                    <label>Username</label>
+                    <input type="text" name="username" class="form-control" value="<?=$a['username']?>">
+                </div>
+                <div class="form-group">
+                    <label>Password</label>
+                    <input type="text" name="password" class="form-control" value="<?=$a['password']?>">
+                </div>
+
+                <a href="index.php" class="btn btn-secondary">Batal</a>
+                <input type="submit" value="Ubah" class="btn btn-primary">
+            </form> 
+        </div>
+    </div>
+</div>
+
+<?php 
+include 'footer.php';
+?>
