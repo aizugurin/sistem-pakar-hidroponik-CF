2f9f2b23ff308d7114b2fb5403f5f7d8ecba6ac2
ENYDAPS <88312902+aizugurin@users.noreply.github.com>
2024-06-25 20:38:04 +0800

    Add files via upload

---
+++ masuk.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title></title>
+    <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
+</head>
+<body>
+
+
+
+    <div class="container">
+        
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+            <div class="col-xl-5 col-lg-12 col-md-9">
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">SILAHKAN MASUK</h1>
+                                    </div>
+                                    <div class="text-center">
+                                    <a href="index.php" class="btn btn-primary btn-block">ADMIN <span class="fas fa-fw fa-user"></span></a>
+                                    <a href="pengguna/daftar.php" class="btn btn-secondary btn-block">DIAGNOSA <span class="fas fa-fw fa-users"></span></a>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+
+
+    <script src="assets/vendor/jquery/jquery.min.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="assets/js/sb-admin-2.min.js"></script>
+</body>
+</html>
+
+
+
+
